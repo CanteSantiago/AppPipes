e85f99eba8928b04bffd0e4b659e68391d329081
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-20 18:28:29 -0500

    Vid154-ModulosYRutas

---
+++ pipesApp/src/app/products/pages/basicsPages/basicsPages.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basicsPages',
+  templateUrl: './basicsPages.component.html',
+  styleUrls: ['./basicsPages.component.css']
+})
+export class BasicsPagesComponent  {
+
+  constructor() { }
+
+
+}
---
+++ pipesApp/src/app/products/pages/numbers-page/numbers-page.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-numbers-page',
+  templateUrl: './numbers-page.component.html',
+  styleUrls: ['./numbers-page.component.css']
+})
+export class NumbersPageComponent  {
+
+  constructor() { }
+
+
+}
