d8132ae1b72d687f91d6999c96828fdab3ccfc86
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-20 11:29:49 -0500

    Vid149-[seccion 11]-Inicio de nuevo proyecto

---
+++ pipesApp/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { AppRouterModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+AppRouterModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
