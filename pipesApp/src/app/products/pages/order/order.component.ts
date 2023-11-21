ff95a40cc06d47f789c97a511cea33eb9f12e941
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-21 11:57:06 -0500

    Vid174-PrimeNG-Table

--- pipesApp/src/app/products/pages/order/order.component.ts
+++ pipesApp/src/app/products/pages/order/order.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
+import { Color, Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-order',
@@ -10,8 +11,36 @@ export class OrderComponent   {
   public menuItems: MenuItem[]=[];
 
 public isUpperCase:boolean=false;
+public heroes: Hero[]=[
+  {
+  name: 'superman',
+  canFly: true,
+color: Color.blue,
+},
+{
+  name: 'Batman',
+  canFly: false,
+color: Color.black,
+},
+{
+  name: 'Daredevil',
+  canFly: false,
+color: Color.red,
+},
+{
+  name: 'robin',
+  canFly: false,
+color: Color.red,
+},
+{
+  name: 'Linterna verde',
+  canFly: false,
+color: Color.red,
+},
+]
 
 toggleUpercase():void{
   this.isUpperCase = !this.isUpperCase;
 }
 }
+
