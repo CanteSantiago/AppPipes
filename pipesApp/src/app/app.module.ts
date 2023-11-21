f5b78772b57eb527d8176adf6c1f55cb6bcd4516
Santiago Cante <114036508+CanteSantiago@users.noreply.github.com>
2023-11-20 21:14:29 -0500

    Vid159-CambiarIdiomaGlobalYManualmente

--- pipesApp/src/app/app.module.ts
+++ pipesApp/src/app/app.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRouterModule } from './app-routing.module';
 import { SharedModule } from './shared/components/menu/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//configuracion del local de la app
+import localeEsHN from '@angular/common/locales/es-HN';
+import localeFrCA from '@angular/common/locales/fr-CA';
 
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEsHN);
+registerLocaleData( localeFrCA );
 
 
 @NgModule({
@@ -18,7 +24,10 @@ AppRouterModule,
 SharedModule,
 BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es-HN' }
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
